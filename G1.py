cbdef24e2d4a0ae3659cedce69ebc13974a8cdda
noonzx2552 <109357368+noonzx2552@users.noreply.github.com>
2023-12-16 20:12:38 +0700

    Update G1.py

--- G1.py
+++ G1.py
@@ -1,21 +1,23 @@
 import random
-player = 0
+user = 0
 botscore = 0
+draw = 0
 for i in range(1,6):
     player = input("[scissors / paper / rock ] :")
     bot = random.choice(['scissors', 'paper', 'rock'])
     print(f"round:{i} player:{player} code:{bot}")
     if player == bot:
         print("draw")
+        draw +=1
     elif player == 'scissors' and bot == 'paper':
         print("playerwin")
-        player +=1
+        user +=1
     elif player == 'scissors' and bot == 'rock':
         print("botwin")
         botscore +=1
     elif player == 'paper' and bot == 'rock':
         print("playerwin")
-        player +=1 
+        user +=1 
     elif player == 'paper' and bot == 'scissors':
         print("botwin")
         botscore +=1
@@ -24,11 +26,11 @@ for i in range(1,6):
         botscore +=1
     elif player == 'rock' and bot == 'scissors':
         print("playerwin")
-        player = player+1
-print(f'socre[player:{player} bot:{botscore}]')
-if player == botscore:
+        user +=1
+print(f'socre[player:{user} bot:{botscore} draw:{draw}]')
+if user == botscore:
     print("-----draw-----")
-elif player > botscore:
+elif user > botscore:
     print("-----playerwin-----")
-elif player < botscore:
+elif user < botscore:
     print("-----botwin-----")
