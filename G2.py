6adea0f18e14d7f3f15e156d7fad2d1847a32ebe
noonzx2552 <109357368+noonzx2552@users.noreply.github.com>
2024-01-14 10:49:21 +0700

    Update G2.py
    didnt endplayer1

--- G2.py
+++ G2.py
@@ -1,53 +1,68 @@
 import random
+
 player = 10
+playerlist = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+scissors = 0
+paper = 0
+rock = 0
+
 while True:
-    langelist = 0
-    scissors = 0
-    paper = 0
-    rock = 0
-    botlist = [0,0,0,0,0,0,0,0,0,0]
-    #user = input("[scissors / paper / rock ] :")
-    user = random.choice(['scissors', 'paper', 'rock'])
-    botlist.append(user)
-    for i in range(9):
-        botlist[i] = random.choice(['scissors', 'paper', 'rock'])
-    for i in botlist:
+    if playerlist[0] == "-":
+        continue
+    else:
+        user = random.choice(['scissors', 'paper', 'rock'])
+        playerlist[0] = user
+
+    for i in range(1, 10):
+        if playerlist[i] == "-":
+            continue
+        else:
+            playerlist[i] = random.choice(['scissors', 'paper', 'rock'])
+
+    for i in playerlist:
         if i == "scissors":
             scissors += 1
         elif i == "paper":
             paper += 1
         elif i == "rock":
             rock += 1
-    if scissors >= 1 and paper >= 1 and rock >= 1:#draw
-        player = player
-        print(botlist)
-        print(f'scissorss:{scissors} paper:{paper} rock:{rock}')
-        print(f"player:{player}")
-    elif scissors == 0 and paper >= 1 and rock >= 1:#paperwin
-        player = player-rock
-        print(botlist)
-        print(f'scissorss:{scissors} paper:{paper} rock:{rock}')
-        print(f"player:{player}")
-        for i in range(10):
-            if botlist[i] == "rock":
-                botlist[i] == "."
-        print(f'player : {player}')
-    elif scissors >= 1 and paper == 0 and rock >= 1:#rockwin
-        player = player-scissors
-        print(botlist)
-        print(f'scissors:{scissors} paper:{paper} rock:{rock}')
-        print(f"player:{player}")
-        for i in range(10):
-            if botlist[i] == "scissors":
-                botlist[i] == '.'
-        print(f'player : {player}')
-    elif scissors >= 1 and paper >= 1 and rock == 0:#scissorswin
-        player = player-paper
-        print(botlist)
-        print(f'scissorss:{scissors} paper:{paper} rock:{rock}')
-        print(f"player:{player}")
-        for i in range(10):
-            if botlist[i] == "paper":
-                botlist[i] = '.'
+
+    if scissors > 0 and paper > 0 and rock > 0:
+        scissors = 0
+        paper = 0
+        rock = 0
+        print(playerlist)
+        print(f"scissors={scissors} paper={paper} rock={rock} player={player}")
+    elif scissors > 0 and paper > 0 and rock == 0:
+        player = player - paper
+        print(playerlist)
+        print(f"scissors={scissors} paper={paper} rock={rock} player={player}")
+        scissors = 0
+        paper = 0
+        rock = 0
+        for i in range(len(playerlist)):
+            if playerlist[i] == 'paper':
+                playerlist[i] = "-"
+    elif scissors > 0 and paper == 0 and rock > 0:
+        player = player - scissors
+        print(playerlist)
+        print(f"scissors={scissors} paper={paper} rock={rock} player={player}")
+        scissors = 0
+        paper = 0
+        rock = 0
+        for i in range(len(playerlist)):
+            if playerlist[i] == 'scissors':
+                playerlist[i] = "-"
+    elif scissors == 0 and paper > 0 and rock > 0:
+        player = player - rock
+        print(playerlist)
+        print(f"scissors={scissors} paper={paper} rock={rock} player={player}")
+        scissors = 0
+        paper = 0
+        rock = 0
+        for i in range(len(playerlist)):
+            if playerlist[i] == 'rock':
+                playerlist[i] = "-"
+
     if player == 1:
         break
